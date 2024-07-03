8edf9d4ff9b3a7b74ac941adbc448143cc6418b6
Manik <tonysins43@gmail.com>
2024-07-03 21:45:27 +0600

    successfully add blog

--- src/components/ui/blogs/AddBlogModal.tsx
+++ src/components/ui/blogs/AddBlogModal.tsx
@@ -3,17 +3,17 @@ import { Button, Modal } from "antd";
 import PForm from "../../form/PFrom";
 import { FieldValues } from "react-hook-form";
 import PInput from "../../form/PInput";
-import PSelect from "../../form/PSelect";
-import { useAddSkillMutation } from "../../../redux/features/skill/skillApi";
 import toast from "react-hot-toast";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import { ApiError } from "../../../types/responseType";
 import UploadButton from "../skills/UploadButton";
 import PTextArea from "../../form/PTextArea";
+import { useAddBlogMutation } from "../../../redux/features/blog/blogApi";
 
 const AddBlogModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [addBlog, { isLoading }] = useAddBlogMutation();
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -28,33 +28,34 @@ const AddBlogModal = () => {
 
   // handle skill add
   const handleAddBlog = async (values: FieldValues) => {
-    // try {
-    //   const skillData = {
-    //     image: imageUrl,
-    //     title: values.title,
-    //     category: values.category,
-    //   };
-    //   const res = await addSkill(skillData);
-    //   console.log(res);
-    //   if (res?.data?.success) {
-    //     toast.success("Skill added successfully");
-    //     handleCancel();
-    //   } else if (res?.error) {
-    //     if ("data" in res.error) {
-    //       // Type assertion to access error data safely
-    //       const errorData = (res.error as FetchBaseQueryError).data as {
-    //         message?: string;
-    //       };
-    //       toast(errorData?.message || "An unknown error occurred");
-    //     } else {
-    //       toast("An unknown error occurred");
-    //     }
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    //   const apiError = error as ApiError;
-    //   toast(apiError?.data.errorMessage || "Something went wrong");
-    // }
+    console.log(values);
+    try {
+      const blogData = {
+        image: imageUrl,
+        title: values.title,
+        content: values.content,
+      };
+      const res = await addBlog(blogData);
+      console.log(res);
+      if (res?.data?.success) {
+        toast.success("Blog added successfully");
+        handleCancel();
+      } else if (res?.error) {
+        if ("data" in res.error) {
+          // Type assertion to access error data safely
+          const errorData = (res.error as FetchBaseQueryError).data as {
+            message?: string;
+          };
+          toast(errorData?.message || "An unknown error occurred");
+        } else {
+          toast("An unknown error occurred");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      const apiError = error as ApiError;
+      toast(apiError?.data.errorMessage || "Something went wrong");
+    }
   };
 
   return (
@@ -82,8 +83,7 @@ const AddBlogModal = () => {
                 }}
                 htmlType="submit"
               >
-                {/* {isLoading ? "Submitting" : "Submit"} */}
-                Submit
+                {isLoading ? "Submitting" : "Submit"}
               </Button>
             </PForm>
           </div>
